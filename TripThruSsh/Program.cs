5a712bc2b4e108531125726f7d161f014e374b46
Daniel Montes de Oca <danielmontesdeoca@Daniels-MacBook-Pro.local>
2014-04-09 23:06:53 -0700

    added request format to work with ie

--- TripThruSsh/Program.cs
+++ TripThruSsh/Program.cs
@@ -14,7 +14,7 @@ namespace TripThruSsh
     class Program
     {
 
-        private static Boolean fullDeploy = false; //if true will upload and replace everything, else just update partner configuations
+        private static Boolean fullDeploy = true; //if true will upload and replace everything, else just update partner configuations
         private static Dictionary<string, Environment> environments = new Dictionary<string, Environment>{
             {"sandbox", new Environment{
                      host = "54.201.134.194",
@@ -45,7 +45,7 @@ namespace TripThruSsh
 
         private static void Main(string[] args)
         {
-            env = environments["vagrant"];
+            env = environments["sandbox"];
             localPath = "Z:\\WindowsDev\\Gateway\\";
             remoteFilePath = "/home/tripservice/servicestack/";
             host = env.host;
