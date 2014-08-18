c871e3c7df98da7ed79217b05e2b2c00319c9341
Daniel Montes de Oca <danielmontesdeoca@Daniels-MacBook-Pro.local>
2014-08-18 07:14:58 -0700

    Changed collection names

--- TripThruSsh/Program.cs
+++ TripThruSsh/Program.cs
@@ -58,8 +58,8 @@ namespace TripThruSsh
 
         private static void Main(string[] args)
         {
-            env = environments["vagrant"];
-            localPath = @"C:\Users\OscarErnesto\Documents\Visual Studio 2013\Projects\Gateway\";
+            env = environments["digital-ocean-mono3"];
+            localPath = @"Z:\WindowsDev\Gateway\";
             remoteFilePath = "/home/tripservice/servicestack/";
             host = env.host;
             user = env.user;
