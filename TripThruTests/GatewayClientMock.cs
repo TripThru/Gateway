e736bbe364badb92b3e6a0cff66860ac3a2eaa72
Daniel Montes de Oca <danielmontesdeoca@Daniels-MacBook-Pro.local>
2014-08-20 00:43:59 -0700

    Removed unnecessary code from GatewayClientMock

--- TripThruTests/GatewayClientMock.cs
+++ TripThruTests/GatewayClientMock.cs
@@ -24,24 +24,13 @@ namespace TripThruTests
             Gateway.RegisterPartnerResponse resp = server.RegisterPartner(gateway, coverage);
             if (resp.result == Gateway.Result.Rejected)
                 rejects++;
-            return new Gateway.RegisterPartnerResponse
-            {
-                result = resp.result
-            };
+            return resp;
         }
 
         public override Gateway.GetPartnerInfoResponse GetPartnerInfo(Gateway.GetPartnerInfoRequest request)
         {
             requests++;
-            Gateway.GetPartnerInfoResponse resp = server.GetPartnerInfo(request);
-
-            Gateway.GetPartnerInfoResponse response = new Gateway.GetPartnerInfoResponse
-            {
-                fleets = resp.fleets,
-                vehicleTypes = resp.vehicleTypes,
-                result = resp.result
-            };
-            return response;
+            return server.GetPartnerInfo(request);
         }
 
         public override Gateway.DispatchTripResponse DispatchTrip(Gateway.DispatchTripRequest request)
@@ -57,11 +46,7 @@ namespace TripThruTests
                 if (resp1.distance != null) distance = distance + resp1.distance.Value;
                 if (resp1.price != null) fare = fare + resp1.price.Value;
             }*/
-            Gateway.DispatchTripResponse response = new Gateway.DispatchTripResponse
-            {
-                result = resp.result,
-            };
-            return response;
+            return resp;
         }
 
         public override Gateway.QuoteTripResponse QuoteTrip(Gateway.QuoteTripRequest request)
@@ -70,57 +55,18 @@ namespace TripThruTests
             Gateway.QuoteTripResponse resp = server.QuoteTrip(request);
             if (resp.result == Gateway.Result.Rejected)
                 rejects++;
-            Gateway.QuoteTripResponse response = new Gateway.QuoteTripResponse
-            {
-                result = resp.result
-            };
-            return response;
+            return resp;
         }
 
         public override Gateway.GetTripStatusResponse GetTripStatus(Gateway.GetTripStatusRequest request)
         {
             requests++;
-            Gateway.GetTripStatusResponse resp = server.GetTripStatus(request);
-            Gateway.GetTripStatusResponse response;
-            if (resp.result == Gateway.Result.OK)
-            {
-                response = new Gateway.GetTripStatusResponse
-                {
-                    result = Gateway.Result.OK,
-                    ETA = resp.ETA,
-                    passengerName = resp.passengerName,
-                    driverID = resp.driverID,
-                    driverLocation = resp.driverLocation,
-                    driverName = resp.driverName,
-                    dropoffTime = resp.dropoffTime,
-                    dropoffLocation = resp.dropoffLocation,
-                    fleetName = resp.fleetName,
-                    fleetID = resp.fleetID,
-                    vehicleType = resp.vehicleType,
-                    status = resp.status,
-                    partnerName = resp.partnerName,
-                    partnerID = resp.partnerID,
-                    pickupTime = resp.pickupTime,
-                    pickupLocation = resp.pickupLocation,
-                    distance = resp.distance,
-                    driverRouteDuration = resp.driverRouteDuration,
-                    price = resp.price
-                };
-            }
-            else
-            {
-                response = new Gateway.GetTripStatusResponse
-                {
-                        result = resp.result
-                };
-            }
-            return response;
+            return server.GetTripStatus(request);
         }
 
         public override Gateway.UpdateTripStatusResponse UpdateTripStatus(Gateway.UpdateTripStatusRequest request)
         {
             requests++;
-            UpdateTripStatusResponse resp = server.UpdateTripStatus(request);
             switch (request.status)
             {
                 case Status.Cancelled:
@@ -130,29 +76,19 @@ namespace TripThruTests
                     completes++;
                     break;
             }
-            Gateway.UpdateTripStatusResponse response;
-            response = new Gateway.UpdateTripStatusResponse
-            {
-                result = resp.result
-            };
-            return response;
+            return server.UpdateTripStatus(request);
         }
 
         public override Gateway.UpdateQuoteResponse UpdateQuote(Gateway.UpdateQuoteRequest request)
         {
             requests++;
-            var resp = server.UpdateQuote(request);
-            return new Gateway.UpdateQuoteResponse(resp.result);
+            return server.UpdateQuote(request);
         }
 
         public override GetQuoteResponse GetQuote(GetQuoteRequest request)
         {
             requests++;
-            var resp = server.GetQuote(request);
-            if (resp.result == Result.OK)
-                return new Gateway.GetQuoteResponse(resp.status, resp.quotes);
-            else
-                return new Gateway.GetQuoteResponse(result: resp.result);
+            return server.GetQuote(request);
         }
     }
 }
