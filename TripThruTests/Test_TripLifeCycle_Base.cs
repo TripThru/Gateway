5f626cf1371f84a4602a7d75a94928861797aeed
Daniel Montes de Oca <danielmontesdeoca@Daniels-MacBook-Pro.local>
2014-10-16 03:03:50 -0700

    Additional logging

--- TripThruTests/Test_TripLifeCycle_Base.cs
+++ TripThruTests/Test_TripLifeCycle_Base.cs
@@ -132,7 +132,7 @@ namespace TripThruTests
                     if (test.Passed == null)
                         passed = null;
                 }
-                Console.WriteLine("Threads: " + (System.Diagnostics.Process.GetCurrentProcess().WorkingSet64 / 1048576).ToString() + "Mb");
+                Console.WriteLine("Threads: " + (System.Diagnostics.Process.GetCurrentProcess().Threads.Count.ToString()));
                 Thread.Sleep(simInterval);
             }
             Test_TripLifeCycle_Base.testsRunning = false;
