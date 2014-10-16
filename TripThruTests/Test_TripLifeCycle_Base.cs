cbeb61ba8bed1822f207cca9316c1e423ab3856b
Daniel Montes de Oca <danielmontesdeoca@Daniels-MacBook-Pro.local>
2014-10-16 00:28:03 -0700

    Additional logging

--- TripThruTests/Test_TripLifeCycle_Base.cs
+++ TripThruTests/Test_TripLifeCycle_Base.cs
@@ -315,7 +315,7 @@ namespace TripThruTests
         {
             var message = "GM " + partnerServiceMock.server.ID + " " + partnerServiceMock.GetHashCode() + " Trip didn't advance from Queued status but wasn't rejected either. Status: "+ trip.status +". Trip: " + trip.ID + " " + trip.GetHashCode();
             if (!partnerServiceMock.RequestsByTripId.ContainsKey(trip.publicID))
-                Thread.Sleep(30000);
+                Thread.Sleep(new TimeSpan(0, 1, 30));
             Assert.IsTrue(partnerServiceMock.RequestsByTripId.ContainsKey(trip.publicID), message + ". No requests received at all.");
             var requests = partnerServiceMock.RequestsByTripId[trip.publicID];
             Assert.GreaterOrEqual(requests.RejectedUpdates, 1, message + "No rejected requests received.");
